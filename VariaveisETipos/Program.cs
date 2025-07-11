b6ab0f46fcdff8acd825bf9408a8c0cf721f900b
Marcelo <fernandesnunes026@gmail.com>
2025-07-11 17:34:29 -0400

    Adicionar arquivos de projeto.

---
+++ VariaveisETipos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tipo Númerico Int");
+int quantidadeProdutos = 42;
+
+int quantidade;
+quantidade = quantidadeProdutos;
+
+//Incremento
+quantidade++;
+//Imprime qunatidade 42
+Console.WriteLine(quantidade);
