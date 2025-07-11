acd0e985f4d6a73efa854f9b7c93d69736ed7f3a
Marcelo <fernandesnunes026@gmail.com>
2025-07-11 17:59:29 -0400

    Inserção de mais tipos de variaveis

--- VariaveisETipos/Program.cs
+++ VariaveisETipos/Program.cs
@@ -20,15 +20,23 @@ byte idade = 25;
 sbyte temperatura = -5;
 short numeroCurto = 1000;
 ushort codigo = 500;
+double numeroComPontoFlutuante = 20.00;
+long numeroGrande = 200000000000000000;
+decimal numeroMuitoGrande = 2000000000000000000;
+
+
 
 //Incremento
 quantidade++;
 //Imprime variaveis
-Console.WriteLine("Quantidade " + quantidade);
-Console.WriteLine("Inicial " + inicial);
-Console.WriteLine("Ativo " + ativo);
-Console.WriteLine("Idade " + idade);
-Console.WriteLine("Temperatura " + temperatura);
-Console.WriteLine("Número curto " + numeroCurto);
-Console.WriteLine("Código " + codigo);
+Console.WriteLine("Quantidade: " + quantidade);
+Console.WriteLine("Inicial: " + inicial);
+Console.WriteLine("Ativo: " + ativo);
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Temperatura: " + temperatura);
+Console.WriteLine("Número curto: " + numeroCurto);
+Console.WriteLine("Código: " + codigo);
+Console.WriteLine("Numero Com Ponto Flutuante: " + numeroComPontoFlutuante);
+Console.WriteLine("Numero grande: " + numeroGrande);
+Console.WriteLine("Numero muito grande: " + numeroMuitoGrande);
 
