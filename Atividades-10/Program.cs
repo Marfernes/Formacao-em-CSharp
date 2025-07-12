c1c6a6aa767a2273fc7706674277ea099c651f1a
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 18:15:20 -0400

    Usando do while fazendo soma de compras de um supermercado

---
+++ Atividades-10/Program.cs
@@ -0,0 +1,14 @@
+﻿// enquanto meu contador for diferente de zero soma o valor digitado
+
+
+int contador;
+int soma = 0;
+do
+{
+    Console.WriteLine("#SUPERMERCADO");
+    Console.WriteLine("Digite valor da venda:");
+    contador = int.Parse(Console.ReadLine());
+    soma += contador;
+} 
+while(contador != 0);
+Console.WriteLine($"Compra finalizada. Total: R$ {soma - 0:F2}");
