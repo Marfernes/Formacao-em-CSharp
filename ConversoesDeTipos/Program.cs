c8f122555331b7adba9fc5a55620356db99acaeb
Marcelo <fernandesnunes026@gmail.com>
2025-07-11 21:00:05 -0400

    Exercicio calculando idade de acordo com ano de nascimento

--- ConversoesDeTipos/Program.cs
+++ ConversoesDeTipos/Program.cs
@@ -11,3 +11,33 @@ byte quantidadeByte = (byte)quantidadeLong;
 
 double valorUnitario = 30.0 / 4;
 Console.WriteLine(valorUnitario);
+
+//Escreva um programa em C# que, a partir do ano de nascimento de uma pessoa, calcule a idade com base no ano atual.
+
+
+
+int anoAtual = DateTime.Now.Year;
+
+Console.WriteLine("Digite ano de Nascimento");
+string entrada = Console.ReadLine();
+
+if(int.TryParse(entrada, out int  anoNascimento))
+{
+    int idade = anoAtual - anoNascimento;
+    if (idade < 0)
+    {
+        Console.WriteLine("Ano de nascimento inválido!");
+    }
+    {
+        Console.WriteLine($"Você tem {idade} anos.");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Digite um valor númerico como 1192");
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+
+
