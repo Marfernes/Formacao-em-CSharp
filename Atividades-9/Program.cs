84fffe00892610580ee17dec49267d8f4c94b3fc
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 17:31:40 -0400

    Autenticação usando switch case

---
+++ Atividades-9/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("#MENU");
+
+Console.WriteLine("SISTEMA DE AUTENTICAÇÂO");
+Console.WriteLine("-----------------------");
+Console.WriteLine("1_ Cadastro novo usuário");
+Console.WriteLine("2_ Acessar como convidado");
+Console.WriteLine("3_ Sair...");
+
+int opcao = int.Parse(Console.ReadLine());
+
+string mensagem = opcao switch
+{
+    1 => "Cadastre um novo usuário",
+    2 => "Acesso como convidado",
+    3 => "Você foi deslogado",
+    _ => "Opção inválida"
+};
+Console.WriteLine($"{mensagem}");
+Console.WriteLine("Nome Usuário: ");
+string usuarioNovo = Console.ReadLine();
+Console.WriteLine($"Usuário {usuarioNovo} cadastrado");
+
+
+
