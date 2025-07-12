d0d6d098ae9d2ddaea196b4317421685889a9edb
Marcelo <fernandesnunes026@gmail.com>
2025-07-11 22:26:57 -0400

    Uso do float

---
+++ Atividades-4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Calculando média de notas de três alunos
+
+float nota1 = 7.2f;
+float nota2 = 8.3f;
+float nota3 = 9.1f;
+float mediaFinal;
+
+mediaFinal = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine(mediaFinal);
