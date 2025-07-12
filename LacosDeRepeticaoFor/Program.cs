5bee45ef17836a92205d0b2c4883d276ce9e0abb
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 19:16:01 -0400

    Uso do For em uma lista de nomes

---
+++ LacosDeRepeticaoFor/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+List<string> funcionarios = new List<string>
+{
+            "João-Gerente-8500",
+            "Maria-Analista de Sistemas-6200",
+            "Carlos-Desenvolvedor-5500",
+            "Ana-Designer-4800",
+            "Pedro-Suporte Técnico-3900",
+            "Fernanda-Coordenadora de Projetos-7300",
+            "Ricardo-Contador-6000",
+            "Juliana-RH-4500"
+};
+for (int i = 0; i < funcionarios.Count; i++)
+{
+    var nome = funcionarios[i].Split('-')[0];
+    if (nome.Equals("Fernanda"))
+    {
+        Console.WriteLine($"Os dados do funcionário é : {funcionarios[i]}");
+        break;
+    }
+}
+
+for (int i = 0; i < funcionarios.Count; i++)
+{
+    var salario = funcionarios[i].Split('-')[2];
+    var salarioInt = int.Parse(salario);
+    if (salarioInt > 5000)
+    {
+        continue;
+    }
+    Console.WriteLine($"Os dados do funcionário é : {funcionarios[i]}");
+}
