40f53f3fe25bb9bd086daeb55defa613ed77e560
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 15:22:26 -0400

    switch case com mensagem personalizadas!

---
+++ Atividades-7/Program.cs
@@ -0,0 +1,19 @@
+﻿//Crie um programa que:
+
+//Receba o cupom de recompensa inserido
+//Retorne uma mensagem de acordo com o cupom
+//Informe que “Recompensa indisponível” se o cupom for inválido
+
+Console.WriteLine("#MENU");
+Console.WriteLine("DIGITE SEU CUPOM: (DOBRAR, CURAR, OURO, ESPECIAL)");
+string cupom = Console.ReadLine();
+
+string mensagem = cupom switch
+{
+    "DOBRAR" => "Ganhar 2x EXP por 1 hora",
+    "CURAR" => "Poção de cura",
+    "OURO" => "100 moedas de ouro",
+    "ESPECIAL" => "Item lendário",
+    _ => "Recompensa indisponível, cupom inválido!"
+};
+Console.WriteLine($"Sua recompensa é {mensagem}");
