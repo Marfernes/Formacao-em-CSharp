4c21b04b60e3054fa9731639055fee05cc6701d8
Marcelo <fernandesnunes026@gmail.com>
2025-07-11 20:21:50 -0400

    ConversoesDeTipos

---
+++ ConversoesDeTipos/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int quantidade = 42;
+// convertendo int para long / conversão implicita
+long quantidadeLong = quantidade;
+
+// conversão explicita
+byte quantidadeByte = (byte)quantidadeLong;
+
+double valorUnitario = 30.0 / 4;
+Console.WriteLine(valorUnitario);
