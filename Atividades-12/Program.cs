7be9983e4825a0fb3a1734b5a580f3a944b83074
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 19:37:10 -0400

    Uso do for para mostrar números de 1 a 22 pulando os multiplos de 3

---
+++ Atividades-12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Lista de participantes (exceto múltiplos de 3):");
+
+for (int i = 1; i <= 22; i++)
+{
+    //Lê-se: "se o resto da divisão de i por 3 for diferente de 0, não é multiplo de 3".
+
+    if (i % 3 != 0)
+    {
+        Console.WriteLine($"Participante nº {i}");
+    }
+}
