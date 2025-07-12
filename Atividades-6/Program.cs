f0e4e12e9120369778c29b0e7098588c758cca5a
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 15:08:57 -0400

    uso do switch case

---
+++ Atividades-6/Program.cs
@@ -0,0 +1,23 @@
+﻿//Para isso, você precisa de um programa que:
+
+//Pergunte ao usuário qual o momento do dia (manhã, tarde ou noite).
+//Solicite o nome do usuário.
+//Exiba uma saudação personalizada com base no momento do dia e no nome fornecido.
+//Exiba uma mensagem em caso de opção inválida.
+
+Console.WriteLine("Que momento do dia é agora ?: Manhã, Tarde, Noite");
+
+string opcao = Console.ReadLine();
+Console.WriteLine("Qual o seu nome?");
+string nome = Console.ReadLine();
+string momentododia = opcao switch
+{
+    "Manhã" => "Bom dia!",
+    "Tarde" => "Boa tarde!",
+    "Noite" => "Boa Noite!",
+    _ => "Opção Inválida",
+};
+Console.WriteLine($"Olá!, {momentododia}, {nome}");
+
+
+
