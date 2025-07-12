092b7e05c23a8f2fbd4a8579cbb4a17e4939acc3
Marcelo <fernandesnunes026@gmail.com>
2025-07-12 18:41:55 -0400

    Usando o do while para percorrer uma lista de nomes

---
+++ Atividades-11/Program.cs
@@ -0,0 +1,28 @@
+﻿//Crie um programa que:
+
+//Defina uma lista de nomes dos alunos.
+//Utilize um loop para percorrer a lista.
+//Interrompa a busca quando o nome desejado for encontrado.
+//Informe se o nome não for encontrado.
+
+string [] alunos = { "Ana", "Carlos", "Bianca", "João", "Marina" };
+string contador;
+int indice;
+
+do
+{
+    Console.WriteLine("Nome dos Alunos, (Ana, Carlos, Bianca, João, Marina");
+    Console.WriteLine("Digite o nome do Aluno que você deseja encontrar:");
+    contador = Console.ReadLine();
+    indice = 0;
+
+    while(indice < alunos.Length && alunos[indice] != contador)
+    {
+        indice++;
+    }
+
+}
+while (indice == alunos.Length);
+
+Console.WriteLine($"Aluno '{contador}' encontrado!");
+
